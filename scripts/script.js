6ae4a0f1f7ebe7276030d6bfaffbb0ff24ff2227
Julian-VS <julian.varelasilva@outlook.com>
2021-07-28 22:54:54 +0200

    Ejercicios en clase - No funciona

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,127 @@
-alert("hello!");
+/*EX0*/
+/*
+alert("Welcome to my page !")
+*/
 
-document.querySelector('#new-todo').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const text = e.target.elements.text.value.trim();
-    alert(text);
+/*EX2*/
+/*
+let myElement = document.querySelector(".button");
+myElement.style.backgroundColor = "red";
+*/
+
+/*EX3*/
+
+let myElement = document.querySelector(".button");
+myElement.style.backgroundColor = "green";
+
+/*EX4*/
+/*
+myEvent = document.querySelector("#new-todo").addEventListener('submit', (e) => {
+    e.preventDefault()
+    const text = e.target.elements.text.value.trim()
+    console.log(text)
 })
+*/
+
+/*EX5*/
+
+let todos =[]
+
+const createTodo = (text) => {
+    todos.push(text)
+}
+
+
+myEvent = document.querySelector("#new-todo").addEventListener('submit', (e) => {
+    e.preventDefault()
+    const text = e.target.elements.text.value.trim()
+    
+
+    if(text.length>0){
+        createTodo(text)
+        e.target.elements.text.value = ''
+    }
+    console.log(todos)
+})
+
+/*EX6*/
+/*
+const generateTodoDOM = (todo) => {
+    const todoEl = document.createElement('label')
+    const containerEl = document.createElement('div')
+    const todoText = document.createElement('span')
+
+
+    // Setup the todo text
+    todoText.textContent = todo
+    containerEl.appendChild(todoText)
+
+    // Setup container
+    todoEl.classList.add('list-item')
+    containerEl.classList.add('list-item__container')
+    todoEl.appendChild(containerEl)
+
+    return todoEl
+}
+*/
+/*EX7*/
+/*
+const renderTodos = (todos) => {
+    const todoList = document.querySelector('#todos')
+    todoList.innerHTML = ''
+
+    todos.forEach((todo) => {
+            todoList.appendChild(generateTodoDOM(todo))
+    })
+}
+*/
+/*EX8*/
+/*
+const renderTodos = (todos) => {
+    const todoList = document.querySelector('#todos')
+    todoList.innerHTML = ''
+
+    if (todos.length > 0) {
+        todos.forEach((todo) => {
+            todoList.appendChild(generateTodoDOM(todo))
+        })
+    } else {
+        const messageEl = document.createElement('p')
+        messageEl.classList.add('empty-message')
+        messageEl.textContent = 'There are no todos to show'
+        todoList.appendChild(messageEl)
+    }
+}
+
+renderTodos(todos);
+
+}
+*/
+
+const generateTodoDOM = (todo) => {
+    const todoEl = document.createElement('label')
+    const containerEl = document.createElement('div')
+    const todoText = document.createElement('span')
+
+    // Setup the todo text
+    todoText.textContent = todo
+    containerEl.appendChild(todoText)
+
+    // Setup container
+    todoEl.classList.add('list-item')
+    containerEl.classList.add('list-item__container')
+    todoEl.appendChild(containerEl)
+
+    // Setup the remove button
+    const removeButton = document.createElement('button')
+    removeButton.textContent = 'remove'
+    removeButton.classList.add('button', 'button--text')
+    todoEl.appendChild(removeButton)
+    removeButton.addEventListener('click', () => {
+        removeTodo(todoText)
+        renderTodos(todos)
+    })
+
+    return todoEl
+}
+
